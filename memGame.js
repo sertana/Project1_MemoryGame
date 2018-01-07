88555b1eab039fa128953e2401df1360bb854f7e
sertana <basnetsanajaya7@gmail.com>
2018-01-07 10:29:44 -0500

    cards flip need settimeout

--- memGame.js
+++ memGame.js
@@ -25,137 +25,100 @@ window.onload = function() {
   function fresh() {
     document.location.reload();
   }
-
+  function addClicks() {
+    if (clicks == 14) {
+      alert("You lost the game. Try again.");
+    } else {
+      clicks++;
+      $("#counter").text(clicks);
+    }
+  }
+  function flipOrVanish() {
+    if (divArray.length == 2) {
+      if (checkArray[0] == checkArray[1]) {
+        divArray[0].removeClass("cards").addClass("hidden");
+        divArray[1].removeClass("cards").addClass("hidden");
+        checkArray = [];
+        divArray = [];
+      } else {
+        if (checkArray[0] != checkArray[1]) {
+          divArray[0].css("background", "darkblue");
+          divArray[1].css("background", "darkblue");
+          checkArray = [];
+          divArray = [];
+        }
+      }
+    }
+  }
   $("#card1").click(function() {
     $("#card1").css("background", newColors[0]);
     divArray.push($("#card1"));
     checkArray.push(newColors[0]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card2").click(function() {
     $("#card2").css("background", newColors[1]);
     divArray.push($("#card2"));
     checkArray.push(newColors[1]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card3").click(function() {
     $("#card3").css("background", newColors[2]);
     divArray.push($("#card3"));
     checkArray.push(newColors[2]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card4").click(function() {
     $("#card4").css("background", newColors[3]);
     divArray.push($("#card4"));
     checkArray.push(newColors[3]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card5").click(function() {
     $("#card5").css("background", newColors[4]);
     divArray.push($("#card5"));
     checkArray.push(newColors[4]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card6").click(function() {
     $("#card6").css("background", newColors[5]);
     divArray.push($("#card6"));
     checkArray.push(newColors[5]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card7").click(function() {
     $("#card7").css("background", newColors[6]);
     divArray.push($("#card7"));
     checkArray.push(newColors[6]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card8").click(function() {
     $("#card8").css("background", newColors[7]);
     divArray.push($("#card8"));
     checkArray.push(newColors[7]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card9").click(function() {
     $("#card9").css("background", newColors[8]);
     divArray.push($("#card9"));
     checkArray.push(newColors[8]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
   $("#card10").click(function() {
     $("#card10").css("background", newColors[9]);
     divArray.push($("#card10"));
     checkArray.push(newColors[9]);
-    if ((checkArray.lenght = 2)) {
-      if (checkArray[0] == checkArray[1]) {
-        divArray[0].addClass("hidden");
-        divArray[1].addClass("hidden");
-        checkArray = [];
-        divArray = [];
-      }
-    }
+    addClicks();
+    flipOrVanish();
   });
-
   console.log(newColors);
 };
